24febed16c3e419c47eabc9fa6109f46edeae1a9
MurrTuck <murrtuck@gmail.com>
2019-07-16 00:06:40 -0600

    Updating git

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -27,4 +27,4 @@ class App extends Component {
 
 export default App;
 
-//7:51
+
