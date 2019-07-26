a9e892ee110dd1583eee9056eb64ef4f4b5f2215
MurrTuck <murrtuck@gmail.com>
2019-07-26 00:34:28 -0600

    Updating Files

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,17 +3,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      message: ""
-    };
-  }
-
-  handleChange(value) {
-    this.setState({ message: value });
-  }
+  
 
   render() {
     return (
