98261d5ddf2d605852303fbf2fb69d35c5f5b27e
MurrTuck <murrtuck@gmail.com>
2019-07-29 23:04:44 -0600

    2:53 - 7/29

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  
+  constructor() {
+    super()
+
+    this.state = {
+      message: "Murray"
+    }
+  }
+
+  handleChange(value) {
+    this.setState({ message: value })
+  }
 
   render() {
     return (
-      <div className="App">
-        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Welcome to React. Don’t start counting until it Hurts</h2>
+        </div>
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+       </p>
+        <input onChange={e => this.handleChange(e.target.value)} />
         <p>{this.state.message}</p>
       </div>
     );
@@ -16,5 +33,7 @@ class App extends Component {
 }
 
 export default App;
+
+// 2:53 - 7/29
 
 
