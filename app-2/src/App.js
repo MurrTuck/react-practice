857d344c284a74fdae9cde91317aa8737d021e7f
MurrTuck <murrtuck@gmail.com>
2019-11-18 17:05:11 -0700

    8:05 - 11/9

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,7 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      color: ['Green', 'Orange', 'Blue', 'Red', 'Purple', 'Yellow']
+    };
+  }
+
   render() {
+    let colorDisplay = this.state.color.map((element, index) => {
+      return <h2 type={index}>{element}</h2>;
+    });
+
     return (
       <div className='App'>
         <div className='App-header'>
@@ -13,6 +25,8 @@ class App extends Component {
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{this.state.color}</p>
+        <p>{colorDisplay}</p>
       </div>
     );
   }
@@ -20,4 +34,4 @@ class App extends Component {
 
 export default App;
 
-// Checking Git
+// 8:05 - 11/9
